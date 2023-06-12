5263c1562eedbd367e1d980aa6da397ede167cea
Vincent <vincentabensour@yahoo.fr>
2023-06-12 14:27:39 +0200

    Update Strapi Version

--- config/middlewares.js
+++ config/middlewares.js
@@ -8,7 +8,7 @@ module.exports = [
         directives: {
           'connect-src': ["'self'", 'https:'],
           'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:','market-assets.strapi.io', 'res.cloudinary.com'],
           upgradeInsecureRequests: null,
         },
       },
